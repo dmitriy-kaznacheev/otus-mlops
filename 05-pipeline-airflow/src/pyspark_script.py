1b3fe9f58dc629b46e099505136217b64adb03a1
Dmitriy Kaznacheev <dk@example.com>
2025-06-18 16:06:54 +0500

    add: DAG

---
+++ 05-pipeline-airflow/src/pyspark_script.py
@@ -0,0 +1,13 @@
+"""
+Script: pyspark_script.py
+Description: PySpark script for testing.
+"""
+
+
+def main():
+    """Main function to execute the PySpark job"""
+    raise ValueError("not implemented")
+
+
+if __name__ == "__main__":
+    main()
